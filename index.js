19caaa997807423897af42fb30feca06f7572cc1
anivash <andr.ivas12@gmail.com>
2023-03-06 15:42:07 +0100

    add template keyboard to the app

--- index.js
+++ index.js
@@ -2,7 +2,53 @@ require('dotenv').config()
 const TelegramBot = require('node-telegram-bot-api');
 const bot = new TelegramBot(process.env.TG_KEY, {polling: true});
 
-bot.on('message', (msg) => {
+const keyboard = [
+    [
+      {
+        text: 'BTC',
+        callback_data: 'btc'
+      }
+    ],
+    [
+      {
+        text: 'ETH',
+        callback_data: 'eth'
+      }
+    ]
+];
+
+bot.on('message', (msg, ) => {
     const chatId = msg.chat.id;
-    bot.sendMessage(chatId, 'Привет, Друг!');
+    bot.sendMessage(chatId, 'Hello, my friend!', {
+        reply_markup: {
+            inline_keyboard: keyboard
+        }
+    });
+});
+
+bot.on('callback_query', (query) => {
+    const chatId = query.message.chat.id;
+    let currency = '';
+  
+    if (query.data === 'btc') {
+        currency = 'BTC';
+    }
+  
+    if (query.data === 'eth') {
+        currency = 'ETH';
+    }
+  
+    if (currency) {
+        bot.sendMessage(chatId, currency, {
+            reply_markup: {
+                inline_keyboard: keyboard
+            }
+        });
+    } else {
+        bot.sendMessage(chatId, `Hm... let's try again?`, {
+            reply_markup: {
+                inline_keyboard: keyboard
+            }
+        });
+    }
 });
